26f1d06c1089525db4221f60403055234ca23a91
Waspoak-Lucas <98618509+Waspoak-Lucas@users.noreply.github.com>
2022-01-29 14:30:11 +0100

    New_Floor

--- Assets/ChangeMaterial.cs
+++ Assets/ChangeMaterial.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class ChangeMaterial : MonoBehaviour
 {
+
+    public Material InitialMaterial;
+    public Material SwitchedMaterial;
+    Renderer rend;
+    private float currentTime = 0f;
+    private float startingTime = 3f;
+    bool neverDone = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = InitialMaterial;
+        currentTime = startingTime;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnCollisionEnter(Collision col)
     {
-        
+        if (col.gameObject.tag == "Ball")
+        {
+            StartCoroutine(colorObjectOverTime(10));
+            neverDone = true;
+        }
+
+    }
+
+    IEnumerator colorObjectOverTime(float duration)
+    {
+        //set color
+        rend.sharedMaterial = SwitchedMaterial;
+        yield return new WaitForSeconds(duration);
+        //reset color
+        rend.sharedMaterial = InitialMaterial;
+        neverDone = false;
     }
 }
+
+
